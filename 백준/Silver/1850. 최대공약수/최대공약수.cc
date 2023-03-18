96c1f51c581e8119aa3a22144735c334e0b155fc
유빈 <51144791+bini59@users.noreply.github.com>
2023-03-19 02:09:25 +0900

    [Silver I] Title: 최대공약수, Time: 504 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.cc
@@ -0,0 +1,78 @@
+//쓸거같은 STL 목록
+#include <iostream>
+#include <vector>
+//#include <queue>
+//#include <stack>
+//#include <cmath>
+//#include <algorithm>
+//#include <string>
+
+using namespace std;
+
+/*
+scanf, printf 쓰기.
+
+cout 을 쓸때는 꼭 endl 말고 \n 쓰기.
+
+아니면 밑에꺼 꼭 쓰기, 시간 개걸림.
+
+*/
+
+/*
+범위 때문에 문제 안풀릴때는, 
+
+index 0은 무시하고 1부터 시작해보기.. 은근 저거때문에 화날때가 많은거같음...
+*/
+
+/*
+꼭 데이터 범위 확인하기!!
+long long 범위 잘 확인해서 풀기.
+*/
+typedef long long ll;
+
+
+//자주 쓸 데이터들
+//vector<vector<int>> pln; // 2차원 배열
+//vector<int> ary; // 1차원 배열
+//queue<int> q; // 큐
+//stack<int> s; // 스택
+
+//for문 간단하게.
+#define FOR(n) for(int i = 0; i < (n); i++)
+
+
+// print Vector<int>
+void printVector(vector<int> arr){
+	FOR((int)arr.size()){
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+}
+
+// print Array<int>
+void printArray(int *arr, int size){
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+ll gcd(ll a, ll b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(){
+	// 혹시나 cin, cout을 쓰게 딘다면...
+	//cin.tie(NULL); 
+	//ios::sync_with_stdio(false);
+    ll a, b;
+    cin >> a >> b;
+
+    for (ll i = 0; i < gcd(a, b); i++){
+        cout << "1";
+    }
+    int result = 0;
+	
+}
