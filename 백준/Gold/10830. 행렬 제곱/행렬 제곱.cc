370e2e1c4a47cf98351f0d2321e37de101a810a0
유빈 <51144791+bini59@users.noreply.github.com>
2023-03-19 02:39:34 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -0,0 +1,107 @@
+//쓸거같은 STL 목록
+#include <iostream>
+#include <vector>
+//#include <queue>
+//#include <stack>
+//#include <cmath>
+//#include <algorithm>
+//#include <string>
+
+using namespace std;
+
+/*
+scanf, printf 쓰기.
+
+cout 을 쓸때는 꼭 endl 말고 \n 쓰기.
+
+아니면 밑에꺼 꼭 쓰기, 시간 개걸림.
+
+*/
+
+
+
+/*
+범위 때문에 문제 안풀릴때는, 
+
+index 0은 무시하고 1부터 시작해보기.. 은근 저거때문에 화날때가 많은거같음...
+*/
+
+
+
+/*
+꼭 데이터 범위 확인하기!!
+long long 범위 잘 확인해서 풀기.
+*/
+typedef long long ll;
+
+
+//자주 쓸 데이터들
+vector<vector<ll>> pln; // 2차원 배열
+//vector<int> ary; // 1차원 배열
+//queue<int> q; // 큐
+//stack<int> s; // 스택
+
+//for문 간단하게.
+#define FOR(n) for(int i = 0; i < (n); i++)
+#define Mod 1000
+
+// 벡터 표시기(int)
+void printVector(vector<int> arr){
+	FOR((int)arr.size()){
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+}
+
+vector<vector<ll>> matrix(vector<vector<ll>> t, vector<vector<ll>> s){
+    int n = (int)s.size();
+    vector<vector<ll>> tt = vector<vector<ll>>(n, vector<ll>(n, 0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            for(int k = 0; k < n; k++){
+                tt[i][j] += (t[i][k]*s[k][j])%Mod;
+            }
+            tt[i][j] %= Mod;
+        }
+    }
+    return tt;
+}
+
+
+
+int main(){
+	// 혹시나 cin, cout을 쓰게 딘다면...
+	//cin.tie(NULL); 
+	//ios::sync_with_stdio(false);
+    ll n, k;
+    scanf("%ld%ld", &n, &k);
+
+
+    vector<vector<ll>> result = vector<vector<ll>>(n, vector<ll>(n, 0));
+    for(int i = 0; i < n; i++){
+        result[i][i] = 1;
+    }
+    pln = vector<vector<ll>>(n, vector<ll>(n, 0));
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &pln[i][j]);
+        }
+    }
+
+    while(k){
+        if(k&1)
+            result = matrix(result, pln);
+        pln = matrix(pln, pln);
+        k = k >> 1;
+    }
+
+	for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%ld ", result[i][j]);
+        }
+        printf("\n");
+    }
+	
+}
