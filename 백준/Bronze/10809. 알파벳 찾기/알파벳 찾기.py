4604d2a8cab9844ecacc330b7e8e90390e1e4efe
유빈 <51144791+bini59@users.noreply.github.com>
2023-03-19 02:38:34 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 60 ms, Memory: 29284 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,9 @@
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+         'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+string = list(input())
+
+for i in alpha:
+    try:
+        print(string.index(i), end=' ')
+    except ValueError:
+        print(-1, end=' ')
