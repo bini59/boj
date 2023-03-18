b5a1e4fcfe6daef1147f83e4eb7532610ff4d450
유빈 <51144791+bini59@users.noreply.github.com>
2023-03-19 02:05:43 +0900

    [Silver V] Title: 날짜 계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.cc
@@ -0,0 +1,88 @@
+//쓸거같은 STL 목록
+#include <iostream>
+#include <vector>
+//#include <queue>
+//#include <stack>
+//#include <cmath>
+//#include <algorithm>
+//#include <string>
+
+using namespace std;
+
+/*
+scanf, printf 쓰기.
+
+cout 을 쓸때는 꼭 endl 말고 \n 쓰기.
+
+아니면 밑에꺼 꼭 쓰기, 시간 개걸림.
+
+*/
+
+
+
+/*
+범위 때문에 문제 안풀릴때는, 
+
+index 0은 무시하고 1부터 시작해보기.. 은근 저거때문에 화날때가 많은거같음...
+*/
+
+
+
+/*
+꼭 데이터 범위 확인하기!!
+long long 범위 잘 확인해서 풀기.
+*/
+//typedef long long ll;
+
+
+//자주 쓸 데이터들
+//vector<vector<int>> pln; // 2차원 배열
+//vector<int> ary; // 1차원 배열
+//queue<int> q; // 큐
+//stack<int> s; // 스택
+
+//for문 간단하게.
+#define FOR(n) for(int i = 0; i < (n); i++)
+
+
+// 벡터 표시기(int)
+void printVector(vector<int> arr){
+	FOR((int)arr.size()){
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+}
+
+
+int main(){
+	// 혹시나 cin, cout을 쓰게 딘다면...
+	//cin.tie(NULL); 
+	//ios::sync_with_stdio(false);
+
+    int a, b, c;
+
+    int x, y, z;
+
+    scanf("%d%d%d", &x, &y, &z);
+
+    int result = 1;
+    a = 1; b = 1; c = 1;
+
+    while(a != x || b != y || c != z){
+        a++;
+        b++;
+        c++;
+        if(a == 16) 
+            a = 1;
+        if(b == 29)
+            b = 1;
+        if(c == 20)
+            c = 1;
+
+        result++;
+    }
+
+	printf("%d", result);
+	
+}
