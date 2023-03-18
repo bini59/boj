e42ad4dea5157b67fceed34e53c835cbb52eb828
유빈 <51144791+bini59@users.noreply.github.com>
2023-03-19 02:54:54 +0900

    [Silver II] Title: N과 M (10), Time: 200 ms, Memory: 35040 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,29 @@
+import sys
+import operator
+
+scan = list(map(int, input().split()))
+num = list(map(int, input().split()))
+num.sort()
+
+result = []
+def nm(n, li, index):
+    global scan, result, num
+    if n == scan[1]:
+        result.append(sorted(li+[num[index[-1]]]))
+        return
+
+
+    for i in range(scan[0]):
+        if i not in index:
+            nm(n+1, li+[num[index[-1]]], index+[i])
+
+
+for i in range(scan[0]):
+    nm(1, [], [i])
+
+result = list(set(map(tuple, result)))
+result.sort()
+for j in result:
+    for k in j:
+        sys.stdout.write(f'{k} ')
+    sys.stdout.write('\n')
