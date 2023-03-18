bd731700e5053280bb5e5aa06934ec28f16458c0
유빈 <51144791+bini59@users.noreply.github.com>
2023-03-19 02:50:34 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 120 ms, Memory: 41084 KB -BaekjoonHub

---
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.cc
@@ -0,0 +1,105 @@
+//쓸거같은 STL 목록
+#include <iostream>
+#include <vector>
+//#include <queue>
+//#include <stack>
+//#include <cmath>
+//#include <algorithm>
+//#include <string>
+
+using namespace std;
+
+/*
+scanf, printf 쓰기.
+
+cout 을 쓸때는 꼭 endl 말고 \n 쓰기.
+
+아니면 밑에꺼 꼭 쓰기, 시간 개걸림.
+
+*/
+
+
+
+/*
+범위 때문에 문제 안풀릴때는, 
+
+index 0은 무시하고 1부터 시작해보기.. 은근 저거때문에 화날때가 많은거같음...
+*/
+
+
+
+/*
+꼭 데이터 범위 확인하기!!
+long long 범위 잘 확인해서 풀기.
+*/
+typedef long long ll;
+
+
+//자주 쓸 데이터들
+//vector<vector<int>> pln; // 2차원 배열
+//vector<int> ary; // 1차원 배열
+//queue<int> q; // 큐
+//stack<int> s; // 스택
+
+//for문 간단하게.
+#define FOR(n) for(int i = 0; i < (n); i++)
+
+
+// Mod = 1000000007
+
+// n, k = map(int, input().split())
+
+// print((fact(n)*dcpow(fact(k), Mod-2)*dcpow(fact(n-k), Mod-2))%Mod)
+
+#define MOD 1000000007
+
+
+// 벡터 표시기(int)
+void printVector(vector<int> arr){
+	FOR((int)arr.size()){
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+}
+
+
+ll dcpow(ll a, int p){
+	ll res = 1;
+	while(p){
+		if(p % 2) res = (a*res) %MOD;
+		a =  a*a %MOD;
+		p >>= 1;
+	}
+
+	return res;
+}
+
+ll facto[5000000];
+
+int main(){
+	// 혹시나 cin, cout을 쓰게 딘다면...
+	cin.tie(NULL); 
+	ios::sync_with_stdio(false);
+	facto[0] = 1;
+	facto[1] = 1;
+	for (int i = 2; i < 5000000;i++){
+		facto[i] = (facto[i - 1] * (i)) % MOD;	
+	}
+
+	int N;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        /* code */
+        int n, k;
+        cin >> n >> k;
+
+		ll A = facto[n];
+		ll B = (dcpow(facto[k], MOD-2)*dcpow(facto[n-k], MOD-2)) %MOD;
+        ll result = (A*B)%MOD;
+
+        cout << result << "\n";
+    }
+}
+
