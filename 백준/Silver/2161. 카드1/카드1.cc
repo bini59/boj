d7ef2e3ca1561227158dfae07c98fe5630d930bd
유빈 <51144791+bini59@users.noreply.github.com>
2023-06-04 14:14:36 +0900

    [Silver V] Title: 카드1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    int n;
+    cin >>n;
+    queue<int> q;
+    for(int i = 0; i < n; i++) q.push(i+1);
+    while(!q.empty()){
+        cout << q.front() << " ";
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+}
